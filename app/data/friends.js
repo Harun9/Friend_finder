c377a6f98f2fd5237ff1030ce47318a03998b1f0
Harun Abdi <harunrush9@gmail.com>
2019-04-30 01:40:19 -0500

    progress

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friend = [
+  {
+    name: "Mohamed Ali",
+    photo: "https://timedotcom.files.wordpress.com/2018/06/muhammad-ali-pardon.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+
+  {
+    name: "beyonce",
+    photo: "https://pixel.nymag.com/imgs/daily/vulture/2018/06/27/27-beyonce-jay-z.w700.h700.jpg",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+
+
+  {
+    name: "Jean-Claude Van Damme",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxAOxHTLa37kl0Ysk6k7Nq-ApwzrDe33OjZ_rDVFuDbaE7e53O.jpg",
+
+    scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+  },
+
+
+
+
+  {
+    name: "Robyn Rihanna Fenty",
+    photo: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fceliashatzman%2Ffiles%2F2017%2F09%2FRihanna-Headshot-1200x1800.jpg",
+
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+
+
+
+];
+
+module.exports = friend;
+
+
